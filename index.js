2479c67431c358feaae88ad5d4ec74b4480e48e8
Mick Thompson <dthompson@gmail.com>
2014-09-22 16:25:21 -0700

    let aws-sdk handle auth, and move bucket to an env var fixes #6

--- index.js
+++ index.js
@@ -1,88 +1,39 @@
 #!/usr/bin/env node
-var npm  = require("npm");
-var AWS = require("aws-sdk");
-var fs = require("fs");
-var config = require('rc')("npm-internal",{acl:"public-read"});
-
+var npm  = require('npm');
+var AWS = require('aws-sdk');
+var fs = require('fs');
 var crypto = require('crypto');
-var shasum = crypto.createHash('sha1');
+var argv = require('minimist')(process.argv.splice(2));
 
-shasum.update((+(new Date()))+Math.random()+"");
-var hash = shasum.digest('hex');
-
-var command = (process.argv.length > 2) ? process.argv[2] : false;
-var option = (process.argv.length > 3) ? process.argv[3] : "";
-
-if(command === "config"){
-    promptForConfig()
-}else if(!config.accessKeyId  || !config.secretAccessKey || !config.bucket || !command ){
-    fs.createReadStream(__dirname + '/usage.md').on('end', function(){
-        if(command === "publish") promptForConfig(); 
-    }).pipe(process.stdout);
-}else if(command === "publish"){
-    packAndDeploy(option);
+if (!process.env.NPMInternalBucket || !argv._[0] ) {
+    fs.createReadStream(__dirname + '/usage.md').pipe(process.stdout);
+}else if(argv._[0] === 'publish') {
+    packAndDeploy(argv._[1] || "");
 }
 
 function packAndDeploy(path){
 
-    var npmPackage = require(process.cwd()+"/package");
-    var packname = npmPackage.name+"-"+npmPackage.version+".tgz";
+    var shasum = crypto.createHash('sha1');
+    shasum.update((+(new Date()))+Math.random()+'');
+    var hash = shasum.digest('hex');
 
-    AWS.config.update(config);
-    s3 =  new AWS.S3();
+    var npmPackage = require(process.cwd()+'/package');
+    var packname = npmPackage.name+'-'+npmPackage.version+'.tgz';
+
+    var s3 =  new AWS.S3();
 
     npm.load(path, function(err){
         npm.commands.pack(path, function(err){
-            console.log("Uploading Package to S3")
-            var opts = {ACL: config.acl,
+            console.log('Uploading Package to S3')
+            var opts = {ACL: process.env.NPMInternalAcl || 'public-read',
                         Body: fs.readFileSync(packname),
-                        Bucket: config.bucket,
-                        Key: "package/"+(packname).substr(0, packname.length-4)+"-"+hash+".tgz"}
+                        Bucket: process.env.NPMInternalBucket,
+                        Key: 'package/'+(packname).substr(0, packname.length-4)+'-'+hash+'.tgz'}
             s3.putObject(opts, function(err, resp){
                 if(err) throw err;
-                console.log("It's Available here:", "https://"+opts.Bucket+".s3.amazonaws.com/"+opts.Key);
+                console.log('package url: https://'+opts.Bucket+'.s3.amazonaws.com/'+opts.Key);
                 fs.unlinkSync(packname);
             });
         });
     });
-}
-
-
-function promptForConfig(){
-
-    process.stdout.write("Let setup your config!  It will be saved at ~/.npm-internalrc \n");
-
-    function getInput(text, cb){
-        process.stdout.write(text);        
-        process.stdin.once("data", function(d){
-            cb(null, d.toString().replace("\n", ""));
-        });
-    }
-
-    var config = {};
-
-    (function load(results, callback) {
-        if (!results.accessKeyId) return getInput("AWS Key: ", function(err, input) {
-            if (err) return callback(err);
-            results.accessKeyId = input;
-            load(results, callback);
-        });
-        if (!results.secretAccessKey) return getInput('AWS Secret: ', function(err, input) {
-            if (err) return callback(err);
-            results.secretAccessKey = input;
-            load(results, callback);
-        });
-        if (!results.bucket) return getInput('S3 Bucket: ', function(err, input) {
-            if (err) return callback(err);
-            results.bucket = input;
-            load(results, callback);
-        });
-
-        callback(null, results);
-    })({}, function(err, config) {
-        fs.writeFileSync(process.env.HOME+"/.npm-internalrc", JSON.stringify(config));
-        process.exit(0);
-    });
-
-
 }
--- usage.md
+++ usage.md
@@ -1,15 +1,16 @@
 npm-internal needs to be configured.
 
-You can place a .npm-internalrc file in your current directory, in your home directory.
-See all the ways to set the config: https://github.com/dominictarr/rc#standards
+Expects the following Env vars:
+
+  NPMInternalBucket=<s3bucket>
+  AWS_ACCESS_KEY_ID=<key>
+  AWS_SECRET_ACCESS_KEY=<secret>
+
+Optional:
+
+  NPMInternalAcl=public-read  # set object acl on s3
 
 Usage:
 
-npm-internal config
-  This prompts you for config options and then saves them to $HOME/.npm-internalrc
-
 npm-internal publish
   This builds a npm package, and uploads it to your s3 bucket.
-
-
-
