fa53ba85c008768a426e920aaeecd3c394b015eb
Tom Lee <thomas.j.lee@gmail.com>
2015-02-10 08:54:05 -0500

    use queue-async instead of async

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var tape = require('tape'),
-    npmi = require('../index.js'),
-    async = require('async'),
+    queue = require('queue-async'),
+    npmi = require('../index.js'),    
     fakeAWS = require('mock-aws-s3'),
     bucket = __dirname + '/local';
 
@@ -56,7 +56,9 @@ tape('prefix collision test', function(t) {
         deleteTestPackage(s3, 'npm-internal-prefix-test-2', cb);
     });
 
-    async.series(tasks, function() {
+    var q = queue(1);
+    tasks.forEach(function(task) { q.defer(task); });
+    q.awaitAll(function(err, result) {
         t.pass('Completed housekeeping.');
     });
 });
@@ -67,30 +69,32 @@ tape('collision test', function(t) {
     var s3 = new fakeAWS.S3();
 
     // ensure a clean slate, then test
-    deleteTestPackage(s3, 'npm-internal-collision-test-1.0.0', function() {
-        async.series([
-            function(cb) {
+    deleteTestPackage(s3, 'npm-internal-collision-test-1.0.0', function() {         
+        var tasks = [];
+
+        tasks.push(function(cb) {
                 // pass the first time
                 npmi.packAndDeploy(s3, bucket, __dirname + '/fixtures/npm-internal-collision-test',
                     function(err, result) { 
                         t.ok((!err && result), 'Uploaded test package successfully to a clean slate'); 
                         cb();
                     });
-            },
-            
-            function(cb) {
+            });
+
+        tasks.push(function(cb) {
                 // fail the second
                 npmi.packAndDeploy(s3, bucket, __dirname + '/fixtures/npm-internal-collision-test',
                     function(err, result) { 
                         t.ok((!err && !result), 'Correctly failed to upload when existing package is present'); 
                         cb();
                     });
-            },
+            });
 
-
-        ], function() {
+        var q = queue(1);
+        tasks.forEach(function(task) { q.defer(task); });
+        q.awaitAll(function(err, result) {
             // clean up after ourselves
-            deleteTestPackage(s3, 'npm-internal-collision-test-1.0.0', function() { t.pass('Cleaned up after ourselves'); });
-        });        
+            deleteTestPackage(s3, 'npm-internal-collision-test-1.0.0', function() { t.pass('Cleaned up after ourselves'); });                   
+        });            
     });
 });
